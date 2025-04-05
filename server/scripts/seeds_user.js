1ea4c1925a41e56b04f656be07e63580db36fbe0
Kaiyuan Ying <26224239+Ky1eYing@users.noreply.github.com>
2025-04-05 17:11:22 -0400

    fix tab to space

--- server/scripts/seeds_user.js
+++ server/scripts/seeds_user.js
@@ -8,44 +8,44 @@ const db = await dbConnection();
 
 const usersToCreate = [
 
-	{
-		//	Display
-		_id: new ObjectId(),
-		userName: "USER1",															// unique, not null
-		name: "UserFirstName UserLastName",											// not null
-		introduction: "I am user1.",												// Less than 200 characters
-		sex: "Male",																// Less than 20 characters
-		email: "user1@gmail.com",													// email format						
-		phone: "+16667778888",														// only number & "+"
-
-		//	Hidden
-		password: "$2a$10$512l1O/5S8DTXRfYNuOvGOd1y7x0ikZOlXU/C6bzYwIZT92zazoI6",	// "admin123", Bcrypt Rounds: 10, not null
-		avatar: "USER1avatar.jpg",													// filename.jpg, png,...
-
-		createdAt: new Date(),														// auto_created
-		updatedAt: new Date(),														// auto_created
-		lastLogin: null,															// auto_created
-		token: null,																// auto_created
-		expiresAt: null,															// auto_created
-	},
     {
-		_id: new ObjectId(),
-		userName: "USER2",															
-		name: "USER2Name",															
-		introduction: null,												
-		sex: "CustomXXXXXX",																
-		email: null,																	
-		phone: null,			
+        //  Display
+        _id: new ObjectId(),
+        userName: "USER1",                                                          // unique, not null
+        name: "UserFirstName UserLastName",                                         // not null
+        introduction: "I am user1.",                                                // Less than 200 characters
+        sex: "Male",                                                                // Less than 20 characters
+        email: "user1@gmail.com",                                                   // email format                        
+        phone: "+16667778888",                                                      // only number & "+"
 
-		password: "$2a$10$512l1O/5S8DTXRfYNuOvGOd1y7x0ikZOlXU/C6bzYwIZT92zazoI6",	// "admin123",
-		avatar: null,
-																		
-		createdAt: new Date(),														
-		updatedAt: new Date(),														
-		lastLogin: new Date(),
-		token: null,																	
-		expiresAt: null,
-	},
+        //  Hidden
+        password: "$2a$10$512l1O/5S8DTXRfYNuOvGOd1y7x0ikZOlXU/C6bzYwIZT92zazoI6",   // "admin123", Bcrypt Rounds: 10, not null
+        avatar: "USER1avatar.jpg",                                                  // filename.jpg, png,...
+
+        createdAt: new Date(),                                                      // auto_created
+        updatedAt: new Date(),                                                      // auto_created
+        lastLogin: null,                                                            // auto_created
+        token: null,                                                                // auto_created
+        expiresAt: null,                                                            // auto_created
+    },
+    {
+        _id: new ObjectId(),
+        userName: "USER2",
+        name: "USER2Name",
+        introduction: null,
+        sex: "CustomXXXXXX",
+        email: null,
+        phone: null,
+
+        password: "$2a$10$512l1O/5S8DTXRfYNuOvGOd1y7x0ikZOlXU/C6bzYwIZT92zazoI6",    // "admin123",
+        avatar: null,
+
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        lastLogin: new Date(),
+        token: null,
+        expiresAt: null,
+    },
 
 ];
 
