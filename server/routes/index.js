e2795e99f4ea16bdc3899f6be3041bfbd3fd01ab
Rakan Li <liruikang0126@outlook.com>
2025-04-05 18:32:50 -0400

    Update routes index to include likes route

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,11 @@
 import eventsRoutes from "./events.js";
 import usersRoutes from "./users.js";
+import likesRoutes from "./likes.js";
 
 const constructorMethod = app => {
 	app.use("/events", eventsRoutes);
 	app.use("/users", usersRoutes);
+	app.use("/likes", likesRoutes);
 
 	app.use("*", (req, res) => {
 		return res.status(404).json({ error: "Route Not found" });
