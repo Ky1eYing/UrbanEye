60a0aaa27a89ff7df9cb2b108f4454c93c19a7a2
Rakan Li <liruikang0126@outlook.com>
2025-04-05 18:31:58 -0400

    Update data index to include likes module

--- server/data/index.js
+++ server/data/index.js
@@ -1,5 +1,7 @@
 import eventsDataFunctions from './events.js';
 import usersDataFunctions from './users.js';
+import likesDataFunctions from './likes.js';
 
 export const eventsData = eventsDataFunctions;
 export const usersData = usersDataFunctions;
+export const likesData = likesDataFunctions;
