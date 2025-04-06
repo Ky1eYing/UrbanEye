b30c422dc5f95cc5dbe74f741fa74da117a5f2fc
Junhe Jiao <jjiao3@stevens.edu>
2025-04-05 21:53:44 -0400

    create comments back-end

--- server/data/index.js
+++ server/data/index.js
@@ -1,7 +1,9 @@
 import eventsDataFunctions from './events.js';
 import usersDataFunctions from './users.js';
+import commentsDataFunctions from './comments.js';
 import likesDataFunctions from './likes.js';
 
 export const eventsData = eventsDataFunctions;
 export const usersData = usersDataFunctions;
+export const commentsData = commentsDataFunctions;
 export const likesData = likesDataFunctions;
